770db583d4354981a3340845eb300831d970f7e8
Alphanikhil <nikku696969@gmail.com>
2025-01-12 22:22:10 +0530

    Update QRCodeDisplay.jsx

--- client/src/components/QRCodeDisplay.jsx
+++ client/src/components/QRCodeDisplay.jsx
@@ -5,7 +5,7 @@ const QRCodeDisplay = () => {
     const [qrCode, setQrCode] = useState('');
 
     useEffect(() => {
-        const ws = new WebSocket('https://heheh-u3h7.vercel.app'); // Ensure the port matches the backend
+        const ws = new WebSocket('https://heheh-v2ev.onrender.com'); // Ensure the port matches the backend
 
         ws.onmessage = (event) => {
             setQrCode(event.data);
