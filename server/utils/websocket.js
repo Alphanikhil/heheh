3f59175b005bfa302b8fd7cf5bf1e5fef27a21c6
Alphanikhil <nikku696969@gmail.com>
2025-01-13 00:53:53 -0800

    Update websocket.js

--- server/utils/websocket.js
+++ server/utils/websocket.js
@@ -1,13 +1,23 @@
 import { WebSocketServer } from 'ws';
 
-const wss = new WebSocketServer({ port: 8080 });
+const initializeWebSocket = (server) => {
+    const wss = new WebSocketServer({ server });
 
-wss.on('connection', (ws) => {
-    console.log('Client connected');
+    wss.on('connection', (ws) => {
+        console.log('Client connected');
 
-    ws.on('close', () => {
-        console.log('Client disconnected');
+        ws.on('close', () => {
+            console.log('Client disconnected');
+        });
+
+        ws.on('message', (message) => {
+            console.log('Received message:', message);
+            // Echo the message back to the client
+            ws.send(message);
+        });
     });
-});
 
-export default wss;
+    console.log('WebSocket server is running');
+};
+
+export default initializeWebSocket;
